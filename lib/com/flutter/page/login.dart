8e0b00e08b367a3fc9e8d44923186dd8d7a18b2f
yangkui <752544765@qq.com>
2018-08-09 21:30:57 +0800

    更新登录界面

---
+++ lib/com/flutter/page/login.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Login extends StatefulWidget {
+  @override
+  LoginState createState() => LoginState();
+}
+
+class LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 100.0, left: 20.0, right: 20.0),
+        child: ListView(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: <Widget>[
+                        Image.asset(
+                          "assets/images/login_icon.png",
+                          width: 70.0,
+                          height: 70.0,
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              "畅聊",
+                              style: TextStyle(fontSize: 25.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    child: Container(
+                      height: 50.0,
+                      margin: EdgeInsets.all(10.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(0, 245, 255, 0.8),
+                      ),
+                      child: Text(
+                        "登录",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Color.fromRGBO(248, 248, 255, 1.0)),
+                      ),
+                    ),
+                    onTapDown: (e) {},
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    child: Text(
+                      "忘记密码？",
+                      style: TextStyle(fontSize: 18.0, color: Colors.green),
+                    ),
+                    onTapDown: (e) {
+                      //找回密码
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      child: Text("新用户注册",
+                          style:
+                              TextStyle(fontSize: 18.0, color: Colors.green)),
+                      onTapDown: (e) {
+                        //注册
+                      },
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+  }
+}
