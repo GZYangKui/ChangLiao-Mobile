7ba498ef23e6ceca50644a195884b977e1ba7c17
GZYangKui <752544765@qq.com>
2018-08-12 23:49:18 +0800

    更改项目结构

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_app/com/splash_screne.dart';
+import 'package:flutter_app/splash_screne.dart';
 
 void main() => runApp(Application());
 
