4204f367b555f3711a378a9ab7d3480c50e822e3
GZYangKui <752544765@qq.com>
2018-08-15 15:06:11 +0800

    更改搜索好友方式

--- lib/com/navigation/page/subpage/search.dart
+++ lib/com/navigation/page/subpage/search.dart
@@ -5,22 +5,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/com/navigation/component/search_item.dart';
 import 'package:flutter_app/com/navigation/utils/constant.dart' as constants;
-import 'package:flutter_app/com/navigation/netwok/socket_handler.dart' as handler;
+import 'package:flutter_app/com/navigation/netwok/socket_handler.dart'
+    as handler;
 
 class Search extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => SearchState();
 }
 
-class SearchState extends State<Search> {
-  List<String> list =[];
-  String _keyword ="";
+class SearchState extends State<Search> with SingleTickerProviderStateMixin {
   GlobalKey<ScaffoldState> key = GlobalKey();
+
   @override
   void initState() {
     super.initState();
-    handler.currentState=this;
+    handler.currentState = this;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,89 +33,49 @@ class SearchState extends State<Search> {
       body: Column(
         children: <Widget>[
           Container(
-            margin: EdgeInsets.only(top: 10.0,left: 3.0,right: 3.0,bottom: 10.0),
+            margin:
+                EdgeInsets.only(top: 10.0, left: 3.0, right: 3.0, bottom: 10.0),
             decoration: BoxDecoration(
               color: Color.fromRGBO(211, 211, 211, 0.8),
             ),
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "输入搜索关键字",
-                      prefixIcon: Icon(Icons.search),
-                    ),
-                    onChanged: (value){
-                      _keyword = value;
-                    },
-                    controller: TextEditingController(text: ""),
-                    onSubmitted: (value){
-                      _keyword = value;
-                      _search();
-                    },
-                  ),
-                ),
-                GestureDetector(
-                  child: Container(
-                    margin: EdgeInsets.only(right: 5.00),
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(255, 255, 255, 1.0),
-                    ),
+                  child: GestureDetector(
                     child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text("搜索"),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.search),
+                            Expanded(
+                              child: Text(
+                                "搜索用户名",
+                                style: TextStyle(fontSize: 20.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
+                    onTapDown: (event) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) => SearchDialog()));
+                    },
                   ),
-                  onTapDown: (e) => _search(),
                 ),
               ],
             ),
           ),
           Expanded(
-            child:list.length==0?_showMenu():_showResult(),
+            child: _showMenu(),
           ),
         ],
       ),
     );
   }
 
-  void _search() {
-    list.clear();
-    if (_keyword != null && _keyword.trim() != "") {
-      var message = {
-        constants.type: constants.search,
-        constants.subtype: constants.info,
-        constants.id: handler.userName,
-        constants.password: handler.password,
-        constants.keyword: _keyword,
-        constants.version: constants.currentVersion
-      };
-      var httpClient = HttpClient();
-      httpClient
-          .put(constants.server, constants.httpPort,
-          "/${constants.search}/${constants.info}")
-          .then((request) {
-        request.write(json.encode(message) + constants.end);
-        return request.close();
-      }).then((response) {
-        response.transform(utf8.decoder).listen((data) {
-            var _result = json.decode(data);
-            if(_result["user"]!=null) {
-              list.clear();
-              list.add(_result["user"]["id"]);
-            }
-            else
-              key.currentState.showSnackBar(SnackBar(content: Text("找不到该用户信息!")));
-            this.setState((){});
-        });
-      });
-      _keyword = "";
-    } else {
-     key.currentState.showSnackBar(SnackBar(content: Text("搜索关键字不能为空!")));
-    }
-  }
-
-  Widget _showMenu(){
+  Widget _showMenu() {
     return ListView(
       children: <Widget>[
         Padding(
@@ -128,7 +89,10 @@ class SearchState extends State<Search> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text("手机联系人",style: TextStyle(fontSize: 22.0),),
+                      Text(
+                        "手机联系人",
+                        style: TextStyle(fontSize: 22.0),
+                      ),
                       Text("添加或邀请通讯录中的好友"),
                     ],
                   ),
@@ -149,7 +113,10 @@ class SearchState extends State<Search> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text("扫码",style: TextStyle(fontSize: 22.0),),
+                      Text(
+                        "扫码",
+                        style: TextStyle(fontSize: 22.0),
+                      ),
                       Text("扫描二维码名片"),
                     ],
                   ),
@@ -162,15 +129,81 @@ class SearchState extends State<Search> {
       ],
     );
   }
-  Widget _showResult(){
-    return ListView.builder(itemBuilder: (BuildContext context,int index)=>UserItem(list[index]),
-      itemCount:list.length,);
-  }
 
   @override
   void dispose() {
     super.dispose();
-    list.clear();
+  }
+}
+
+class SearchDialog extends StatefulWidget {
+  @override
+  SearchDialogState createState() => SearchDialogState();
+}
+
+class SearchDialogState extends State<SearchDialog> {
+  List<String> list = [];
+  String _keyword = "";
+  GlobalKey<ScaffoldState> key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: key,
+      appBar: AppBar(
+        title: TextField(
+          style: TextStyle(fontSize: 20.0),
+          decoration: InputDecoration(hintText: "搜索用户名"),
+          onSubmitted: (value) {
+            _keyword = value;
+            _search();
+          },
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () => Navigator.pop(context),
+          padding: EdgeInsets.all(0.0),
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) => UserItem(list[index]),
+        itemCount: list.length,
+      ),
+    );
   }
 
+  void _search() {
+    if (_keyword != null && _keyword.trim() != "") {
+      list.clear();
+      var message = {
+        constants.type: constants.search,
+        constants.subtype: constants.info,
+        constants.id: handler.userName,
+        constants.password: handler.password,
+        constants.keyword: _keyword,
+        constants.version: constants.currentVersion
+      };
+      var httpClient = HttpClient();
+      httpClient
+          .put(constants.server, constants.httpPort,
+              "/${constants.search}/${constants.info}")
+          .then((request) {
+        request.write(json.encode(message) + constants.end);
+        return request.close();
+      }).then((response) {
+        response.transform(utf8.decoder).listen((data) {
+          var _result = json.decode(data);
+          if (_result["user"] != null) {
+            list.clear();
+            list.add(_result["user"]["id"]);
+          } else {
+            key.currentState.showSnackBar(SnackBar(content: Text("该用户不存在")));
+          }
+          this.setState(() {});
+        });
+      });
+      _keyword = "";
+    }
+  }
 }
