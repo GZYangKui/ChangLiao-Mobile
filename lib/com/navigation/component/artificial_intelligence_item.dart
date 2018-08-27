e9b7b4dbc019487912434f4deed5cd4495e066ff
GZYangKui <752544765@qq.com>
2018-08-27 21:53:11 +0800

    fix bug

---
+++ lib/com/navigation/component/artificial_intelligence_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class ArtificialIntelligenceItem extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ArtificialIntelligenceItemState();
+}
+
+class ArtificialIntelligenceItemState
+    extends State<ArtificialIntelligenceItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[],
+    );
+  }
+}
